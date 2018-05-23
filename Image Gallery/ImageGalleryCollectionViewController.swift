e5b496354ba4a4893dbda1f37fd8cda715898c24
Alwin Firmansyah <alwinfirmansyah@gmail.com>
2018-05-22 21:09:01 -0700

    edit fetching URL into collection view cell

--- Image Gallery/ImageGalleryCollectionViewController.swift
+++ Image Gallery/ImageGalleryCollectionViewController.swift
@@ -12,67 +12,32 @@ private let reuseIdentifier = "imageCell"
 
 class ImageGalleryCollectionViewController: UICollectionViewController {
     
-//    var imageURLs = [URL]()
-//    var imageAspectRatios = [Double]()
-//
-//    var singleImageURL: URL? {
-//        didSet {
-//            image = nil
-//            if view.window != nil {
-//                fetchImage()
-//            }
-//        }
-//    }
-//
-//    override func viewDidAppear(_ animated: Bool) {
-//        super.viewDidAppear(animated)
-//        if imageView.image == nil {
-//            fetchImage()
-//        }
-//    }
-//
-//    var imageView = UIImageView()
-//
-//    private var image: UIImage? {
-//        get {
-//            return imageView.image
-//        }
-//        set {
-//            imageView.image = newValue
-//            imageView.sizeToFit()
-////            spinner?.stopAnimating()
-//        }
-//    }
-//
-//    // need to eventually include parameters for fetch image so that we know which url to fetch an image for in the url array
-//    private func fetchImage() {
-//        if let url = singleImageURL {
-////            spinner.startAnimating()
-//            DispatchQueue.global(qos: .userInitiated).async {
-//                let urlContents = try? Data(contentsOf: url)
-//                DispatchQueue.main.async { [weak self] in
-//                    if let imageData = urlContents, url == self?.singleImageURL {
-//                        self?.image = UIImage(data: imageData)
-//                    }
-//                }
-//            }
-//        }
-//    }
+    // MARK: model below
     
-    
-    var imageViews = [UIImage(named: "bistro")]
+    var imageURLs = [URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg"),URL(string: "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")]
+    var imageAspectRatios = [Double]()
+    var imageViews = [UIImageView]()
+    var images = [UIImage]()
     
     // MARK: - UICollectionViewDataSource
     
     override func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return imageViews.count
+        return imageURLs.count
     }
     
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         if let imageCell = cell as? ImageCollectionViewCell {
-            let image = imageViews[indexPath.item]
-            imageCell.imageView?.image = image
+            if let url = imageURLs[indexPath.item] {
+                DispatchQueue.global(qos: .userInitiated).async {
+                    let urlContents = try? Data(contentsOf: url)
+                    DispatchQueue.main.async { [weak self] in
+                        if let imageData = urlContents, url == self?.imageURLs[indexPath.item] {
+                            imageCell.imageView?.image = UIImage(data: imageData)
+                        }
+                    }
+                }
+            }
         }
         return cell
     }
