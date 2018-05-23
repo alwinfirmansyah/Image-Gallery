78163ac42ac15c414855f9632db028417bdd1793
Alwin Firmansyah <alwinfirmansyah@gmail.com>
2018-05-22 21:27:04 -0700

    added url extension for google images help

--- Image Gallery/ImageGalleryCollectionViewController.swift
+++ Image Gallery/ImageGalleryCollectionViewController.swift
@@ -14,10 +14,8 @@ class ImageGalleryCollectionViewController: UICollectionViewController {
     
     // MARK: model below
     
-    var imageURLs = [URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg"),URL(string: "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg")]
+    var imageURLs = [URL(string: "https://upload.wikimedia.org/wikipedia/commons/5/55/Stanford_Oval_September_2013_panorama.jpg"),URL(string: "https://www.jpl.nasa.gov/images/cassini/20090202/pia03883-full.jpg"), URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcReIoYqxU5APOY1fPGRuzLX7x47TsnyakYQXp6dLnNytz3k-2te"), URL(string: "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRKzBggXsaBjPSlrLnBjDGg6Go6PxUkMph5P2wsjuruPhBA3qBd")]
     var imageAspectRatios = [Double]()
-    var imageViews = [UIImageView]()
-    var images = [UIImage]()
     
     // MARK: - UICollectionViewDataSource
     
@@ -28,7 +26,7 @@ class ImageGalleryCollectionViewController: UICollectionViewController {
     override func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
         let cell = collectionView.dequeueReusableCell(withReuseIdentifier: reuseIdentifier, for: indexPath)
         if let imageCell = cell as? ImageCollectionViewCell {
-            if let url = imageURLs[indexPath.item] {
+            if let url = imageURLs[indexPath.item]?.imageURL {
                 DispatchQueue.global(qos: .userInitiated).async {
                     let urlContents = try? Data(contentsOf: url)
                     DispatchQueue.main.async { [weak self] in
